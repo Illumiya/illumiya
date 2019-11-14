d6ce97a0e37b669bd20c45e74a70100c339c8fa0
Karthik <developer4python@gmail.com>
2019-11-14 15:06:17 +0530

    Updated the blog comments

--- illumiya/core/urls.py
+++ illumiya/core/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('blog/detail/<int:pk>/', views.BlogDetailView.as_view(), name='blog-detail'),
     path('contact-us/', views.ContactView.as_view(), name='contact-us'),
     path('create/comment/', views.CreateCommentView.as_view(), name='create-comment'),
-    #path('comment/detail/', views.ContactView.as_view(), name='contact-us'),
+    path('sub-comment/<int:pk>/', views.AjaxCommentDetailView.as_view(), name='sub-comment-detail'),
+    path('main-comment/<int:object_pk>/', views.AjaxCommentDetailView.as_view(), name='main-comment-detail'),
 ]
